00253aa5c4c9049dd58b4f0ba5e80c813e79a93b
Steven Leonardo <stevenleonardo@knights.ucf.edu>
2020-04-16 16:52:56 -0400

    schema made

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ("Double Cheese Burger", true);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Bacon Burger", true);
+
+INSERT INTO burgers (burger_name) VALUES ("Tofu Burger");
+INSERT INTO burgers (burger_name) VALUES ("Veggie Burger");
