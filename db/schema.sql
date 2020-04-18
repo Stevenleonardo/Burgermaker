82c2c1530eff493a3707e3b8030d5b0b1ac097cf
Steven Leonardo <stevenleonardo@knights.ucf.edu>
2020-04-17 20:55:48 -0400

    update

--- db/schema.sql
+++ db/schema.sql
@@ -6,7 +6,6 @@ USE burgers_db;
 CREATE TABLE burgers (
     id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
     burger_name VARCHAR(255) NOT NULL,
-    devour BOOLEAN DEFAULT false
+    devour BOOLEAN DEFAULT false,
     createdat TIMESTAMP NOT NULL
-    PRIMARY KEY (id)
 );
